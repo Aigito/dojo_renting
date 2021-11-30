516149f3535ccb4109d2ba247430c77cc2a005ea
Dylan Pee <peedylan94@gmail.com>
2021-11-30 20:22:28 +1030

    created routes for each model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/dashboard', to: 'pages#dashboard'
+
+  resources :dojo_spaces do
+    resources :bookings, except: %i[edit destroy] do
+      resources :reviews, only: %i[create]
+    end
+  end
 end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
