0b5e186144dc4cd5e571dfe887b5bf3a6351953d
Dylan Pee <peedylan94@gmail.com>
2024-02-08 11:47:10 +1030

    TEST

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Cleaning database..."
+puts "Cleaning databasesss..."
 
 User.destroy_all
 DojoSpace.destroy_all
